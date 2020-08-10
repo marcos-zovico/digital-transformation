b3c052c54fa767558888f12e974da2f282fdd436
Marcos <marcosantoniozovico@hotmail.com>
2020-08-09 23:24:35 -0300

    first commit

---
+++ src/main/java/com/msouza/digitaltransformation/DigitalTransformationApplication.java
@@ -0,0 +1,13 @@
+package com.msouza.digitaltransformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTransformationApplication.class, args);
+	}
+
+}
