b4984cfe67d2c31748940c6f0bba313e33397140
Marcos <marcosantoniozovico@hotmail.com>
2020-11-17 00:42:03 -0300

    Adicionando modulos

--- src/main/java/com/msouza/sigo/ServletInitializer.java
+++ src/main/java/com/msouza/sigo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.msouza.digitaltransformation;
+package com.msouza.sigo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DigitalTransformationApplication.class);
+		return application.sources(SigoApplication.class);
 	}
 
 }
